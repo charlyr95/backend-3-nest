672e1cdb3c99a779abe0fada1e06c6da313a4b13
Carlos <carloscr95r@gmail.com>
2026-03-03 14:00:30 -0300

    base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule, ProductsModule, AuthModule],
   controllers: [],
   providers: [],
 })
