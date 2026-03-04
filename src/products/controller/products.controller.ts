b42ef055f9062051ef0cf8bbf5a90409b0151700
Carlos <carloscr95r@gmail.com>
2026-03-04 09:55:50 -0300

    update

--- src/products/controller/products.controller.ts
+++ src/products/controller/products.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from '../services/products.service';
+import { CreateProductDto } from '../dtos/create-product.dto';
+import { UpdateProductDto } from '../dtos/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
