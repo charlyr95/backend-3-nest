e03084538d59d5781dd2ae26988550c2047f138a
Carlos Daniel Romero <carloscr95r@gmail.com>
2026-03-07 13:45:31 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
